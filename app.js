726db84cd03149d701727eae844f540f32a899f7
Gennadiy Pokidov <faqvsem2@yandex.ru>
2018-11-22 01:22:30 +0700

    add gsap

--- app.js
+++ app.js
@@ -1,42 +1,189 @@
 'use strict'
 
 $(document).ready(function () {
-	var swiper = new Swiper('.swiper-container', {
-		direction: 'vertical',
-		slidesPerView: 1,
-		spaceBetween: 0,
-		mousewheel: true,
-		effect: 'fade',
-		speed: 1000,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span class="owl">' + (index + 1) + '</span>';
-			},
-		},
-		navigation: {
-			nextEl: '.button-next',
-			prevEl: '.button-prev',
-		},
+
+	var $layer_0 = $('.layer-0'),
+		$layer_1 = $('.layer-1'),
+		$layer_2 = $('.layer-2'),
+		$x_axis = $('#x-axis'),
+		$y_axis = $('#y-axis'),
+		$container = $('body'),
+		container_w = $container.width(),
+		container_h = $container.height();
+
+	$(window).on('mousemove.parallax', function (event) {
+		var pos_x = event.pageX,
+			pos_y = event.pageY,
+			left = 0,
+			top = 0;
+
+		left = container_w / 2 - pos_x;
+		top = container_h / 2 - pos_y;
+
+		// $(window).resize(function() {
+		// 	left = container_w / 2 - pos_x;
+		// 	top = container_h / 2 - pos_y;
+		// });
+
+		TweenMax.to(
+			$x_axis,
+			1, {
+				css: {
+					transform: 'translateX(' + (left * -1) + 'px)'
+				},
+				ease: Expo.easeOut,
+				overwrite: 'all'
+			});
+
+		TweenMax.to(
+			$y_axis,
+			1, {
+				css: {
+					transform: 'translateY(' + (top * -1) + 'px)'
+				},
+				ease: Expo.easeOut,
+				overwrite: 'all'
+			});
+
+		TweenMax.to(
+			$layer_2,
+			1, {
+				css: {
+					transform: 'translateX(' + left / 12 + 'px) translateY(' + top / 6 + 'px)'
+				},
+				ease: Expo.easeOut,
+				overwrite: 'all'
+			});
+
+		TweenMax.to(
+			$layer_1,
+			1, {
+				css: {
+					transform: 'translateX(' + left / 4 + 'px) translateY(' + top / 2 + 'px)'
+				},
+				ease: Expo.easeOut,
+				overwrite: 'all'
+			});
+
+		TweenMax.to(
+			$layer_0,
+			10, {
+				css: {
+					transform: 'rotate(' + left / 200 + 'deg)'
+				},
+				ease: Expo.easeOut,
+				overwrite: 'none'
+			}
+		)
 	});
 
+	$(window).resize(function() {
+		var container_w = $container.width(),
+		container_h = $container.height();
 
-	// $("body").mousemove(function(e) {
-	// 	let moveX = e.pageX * -1 / 25 + 'px';
-	// 	let moveY = e.pageY * -1 / 25 + 'px';
-	// 	$('.swiper-slide-active').css('background-position', 'calc(50% + '+ moveX + ') calc(50% + ' + moveY + ')');
+		$(window).on('mousemove.parallax', function (event) {
+			var pos_x = event.pageX,
+				pos_y = event.pageY,
+				left = 0,
+				top = 0;
+	
+			left = container_w / 2 - pos_x;
+			top = container_h / 2 - pos_y;
+	
+			// $(window).resize(function() {
+			// 	left = container_w / 2 - pos_x;
+			// 	top = container_h / 2 - pos_y;
+			// });
+	
+			TweenMax.to(
+				$x_axis,
+				1, {
+					css: {
+						transform: 'translateX(' + (left * -1) + 'px)'
+					},
+					ease: Expo.easeOut,
+					overwrite: 'all'
+				});
+	
+			TweenMax.to(
+				$y_axis,
+				1, {
+					css: {
+						transform: 'translateY(' + (top * -1) + 'px)'
+					},
+					ease: Expo.easeOut,
+					overwrite: 'all'
+				});
+	
+			TweenMax.to(
+				$layer_2,
+				1, {
+					css: {
+						transform: 'translateX(' + left / 12 + 'px) translateY(' + top / 6 + 'px)'
+					},
+					ease: Expo.easeOut,
+					overwrite: 'all'
+				});
+	
+			TweenMax.to(
+				$layer_1,
+				1, {
+					css: {
+						transform: 'translateX(' + left / 4 + 'px) translateY(' + top / 2 + 'px)'
+					},
+					ease: Expo.easeOut,
+					overwrite: 'all'
+				});
+	
+			TweenMax.to(
+				$layer_0,
+				10, {
+					css: {
+						transform: 'rotate(' + left / 200 + 'deg)'
+					},
+					ease: Expo.easeOut,
+					overwrite: 'none'
+				}
+			)
+		});
+	});
+
+	// var swiper = new Swiper('.swiper-container', {
+	// 	direction: 'vertical',
+	// 	slidesPerView: 1,
+	// 	spaceBetween: 0,
+	// 	mousewheel: true,
+	// 	effect: 'fade',
+	// 	speed: 1000,
+	// 	pagination: {
+	// 		el: '.swiper-pagination',
+	// 		clickable: true,
+	// 		renderBullet: function (index, className) {
+	// 			return '<span class="owl">' + (index + 1) + '</span>';
+	// 		},
+	// 	},
+	// 	navigation: {
+	// 		nextEl: '.button-next',
+	// 		prevEl: '.button-prev',
+	// 	},
 	// });
-	var startX = -100,
-		startY = -100,
-		w = document.documentElement.offsetWidth,
-		h = document.documentElement.offsetHeight;
 
-	$('.swiper-slide-active').on('mousemove', function (evt) {
-		var posX = Math.round(evt.clientX / w * startX)
-		var posY = Math.round(evt.clientY / h * startY)
-		$(this).css('background-position', posX + 'px ' + posY + 'px');
-	})
+
+	// // $("body").mousemove(function(e) {
+	// // 	let moveX = e.pageX * -1 / 25 + 'px';
+	// // 	let moveY = e.pageY * -1 / 25 + 'px';
+	// // 	$('.swiper-slide-active').css('background-position', 'calc(50% + '+ moveX + ') calc(50% + ' + moveY + ')');
+	// // });
+	// var startX = -100,
+	// 	startY = -100,
+	// 	w = document.documentElement.offsetWidth,
+	// 	h = document.documentElement.offsetHeight;
+
+	// $('.swiper-slide-active').on('mousemove', function (evt) {
+	// 	var posX = Math.round(evt.clientX / w * startX)
+	// 	var posY = Math.round(evt.clientY / h * startY)
+	// 	$(this).css('background-position', posX + 'px ' + posY + 'px');
+	// })
 
 });
 
